d6fc6799d9e0c5ae91079b93c77da188ae33bb92
Seb Jones <jonesseb100@gmail.com>
2021-06-16 16:10:56 +0100

    File for constants

---
+++ constant.py
@@ -0,0 +1,9 @@
+# constant.py
+# Physical constants used
+
+COULOMBCHARGE = 1.602176634*10**-19 # Coulombs
+PI = numpy.pi
+VACPERM = 8.8541878128*10**-12 # F/m - Farads per metre - Vacuum permittivity
+CLIGHT = 299792458 # Speed of light in m/s
+ERESTMASS = 9.10938356*10**-31 # kg
+

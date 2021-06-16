5926b5db7fe97b385a20b2b2d28f8900d126a0a3
Seb Jones <jonesseb100@gmail.com>
2021-06-16 16:11:10 +0100

    A couple of functions

---
+++ singleElectronRadiation.py
@@ -0,0 +1,17 @@
+import numpy
+import matplotlib.pyplot as pyplot
+import constant
+
+# Function definitions
+
+def CalcAngCyclotronFreq(BField, KE):
+    # Angular cyclotron frequency from magnetic field and electron KE    
+    freq = (constant.COULOMBCHARGE * BField)/(constant.ERESTMASS + KE/constant.CLIGHT)
+    return freq
+
+def CalcCylcotronFreq(BField, KE)
+    # Angular cyclotron frequency from magnetic field and electron KE
+    freq = (constant.COULOMBCHARGE * BField)/((constant.ERESTMASS + KE/(constant.CLIGHT*constant.CLIGHT)) * 2 * constant.PI)
+    return freq
+
+

46e5a6bdac6f28c00e93e4db9804a22084a64bed
Seb Jones <jonesseb100@gmail.com>
2021-06-18 12:04:36 +0100

    Added harmonic trap fields

--- MagneticFieldPlots.py
+++ MagneticFieldPlots.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 # MagneticFieldPlots.py
 # Make plots of the magnetic field using the bathtub approximation
 
@@ -49,20 +50,46 @@ def BFieldBathtubPlusBkg(x, p):
         
     return field
 
+def BFieldHarmonic(x, p):
+    # Harmonic trap using a single coil
+    # zPos is position in field
+    # l0 is measure of scale of curvature
+    # b0 is field scale
+    zPos = x[0]
+    l0 = p[0]
+    b0 = p[1]
+
+    field = b0 * (1 + zPos*zPos/(l0*l0))
+
+    return field
+
+def BFieldHarmonicPlusBkg(x, p):
+    # Harmonic trap using a single coil with a background field
+    # zPos is position in field
+    # l0 is measure of scale of curvature
+    # b0 is field scale
+    zPos = x[0]
+    l0 = p[0]
+    b0 = p[1]
+    bBkg = p[2]
+
+    field = b0 * (1 + zPos*zPos/(l0*l0)) + bBkg
+
+    return field
+
 def RunMagneticFieldPlots():
     BKGFIELD = 1. # Tesla
     
-    f = ROOT.TF1("fBathtub; z [cm]; Total field - main field [mT]", BFieldBathtub, -40., 40., 3)
-    f.SetParameter(0, 4.5)
-    f.SetParameter(1, 20)
-    f.SetParameter(2, 0.1)
-    f.SetNpx(1000)
+    f1 = ROOT.TF1("fBathtub; z [cm]; Total field - main field [mT]", BFieldBathtub, -40., 40., 3)
+    f1.SetParameter(0, 4.5)
+    f1.SetParameter(1, 20)
+    f1.SetParameter(2, 0.1)
+    f1.SetNpx(1000)
     
     #c1 = ROOT.TCanvas("c1", "c1")
     #c1.cd()
     #f.Draw("lr")
     
-
     f2 = ROOT.TF1("fBathtubBkg; z [cm]; Total field [T]", BFieldBathtubPlusBkg, -40., 40., 4)
     f2.SetParameter(0, 4.5)
     f2.SetParameter(1, 20)
@@ -70,7 +97,13 @@ def RunMagneticFieldPlots():
     f2.SetParameter(3, 1.)
     f2.GetYaxis().SetRangeUser(0.9, 1)
     f2.SetNpx(1000)
-    f2.Draw("lr")
+    #f2.Draw("lr")
+
+    fHarm = ROOT.TF1("fHarm; z [cm]; Total field [mT]", BFieldHarmonic, -2., 2., 2)
+    fHarm.SetParameter(0, np.sqrt(1000.))
+    fHarm.SetParameter(1, 1)
+    fHarm.SetNpx(1000)
+    fHarm.Draw("lr")
     
     text = input()
     

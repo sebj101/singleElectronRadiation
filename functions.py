9a70f40b158ed3f2f0132d528331fdb0766f4390
Seb Jones <jonesseb100@gmail.com>
2021-06-21 12:29:47 +0100

    Added functions for longitudinal velocity and Doppler shifted frequency

--- functions.py
+++ functions.py
@@ -33,6 +33,7 @@ def BFieldBathtubPlusBkg(zPos, l0, l1, b0, bBkg):
     return field
 
 ### In a "harmonic trap" ########
+### Here we assume there is no energy loss as radiation
 def BFieldHarmonicFromPos(zPos, l0, b0):
     field = b0 * (1 + zPos**2/l0**2)
     return field
@@ -53,9 +54,21 @@ def ZPositionHarmonic(time, v0, l0, b0, bBkg, trapDepth):
 
 def AngCyclFreqHarmonicFromTime(time, ke, v0, l0, b0, bBkg, trapDepth):
     premult = CalcAngCyclotronFreq(bBkg, ke)
-    print(premult)
     thetaBot = np.arcsin(np.sqrt(1 - trapDepth/bBkg))
     axFreq = v0 * np.sin(thetaBot) / l0
     zMax = l0 * 1/np.tan(thetaBot)
     freq = premult * (1 + zMax*zMax/(2*l0*l0) - zMax*zMax/(2*l0*l0)*np.cos(2*axFreq*time))
     return freq
+
+def ZVelocityHarmonic(time, v0, l0, b0, bBkg, trapDepth):
+    thetaBot = np.arcsin(np.sqrt(1 - trapDepth/bBkg))
+    axFreq = v0 * np.sin(thetaBot) / l0
+    zMax = l0 * 1/np.tan(thetaBot)
+    v = zMax * axFreq* np.cos(axFreq * time) / 100.
+    return v
+
+def ReceiverFreqDoppler(vel, f0):
+    # vel: Longitudinal velocity towards receiver
+    # f0: Frequency at source
+    f_r = np.sqrt( (1 - vel / constant.CLIGHT)/(1 + vel / constant.CLIGHT) ) * f0
+    return f_r

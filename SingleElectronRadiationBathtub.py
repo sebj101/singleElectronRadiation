6352ac7dcd5a88b3f62ab52ae1be98d6218eaac3
Seb Jones <jonesseb100@gmail.com>
2021-06-22 16:54:45 +0100

    Added more functions for position but still can't produce those dang sidebands

--- SingleElectronRadiationBathtub.py
+++ SingleElectronRadiationBathtub.py
@@ -12,19 +12,21 @@ def RunSingleElectronBathtubSim():
     
     # Field constants for harmonic trap field
     BBKG = 1. # Tesla
-    L0   = 10. # cm
-    L1   = 15. # cm
+    L0   = 35. # cm
+    L1   = 10. # cm
     B0   = 1. # Tesla
     TRAPDEPTH = 0.004 # Tesla
 
-    DETPOS = np.array([5., 5.]) # Position of receiver
+    DETPOS = np.array([20., 0.]) # Position of receiver
     
     EKE = (18.6 * 10**3) * constant.COULOMBCHARGE # Electron KE in Joules
-    V0 = 0.263 * constant.CLIGHT
-    GAMMA = 1. / np.sqrt(1 - (V0/constant.CLIGHT)**2 )
+    GAMMA = (constant.ERESTMASS*constant.CLIGHT*constant.CLIGHT + EKE)/(constant.ERESTMASS*constant.CLIGHT*constant.CLIGHT)
+    V0 = constant.CLIGHT * ( np.sqrt((GAMMA*GAMMA - 1)/GAMMA) )
+
+    #1. / np.sqrt(1 - (V0/constant.CLIGHT)**2 )
 
     print("GAMMA = ", GAMMA)
-
+    print("BETA = ", V0/constant.CLIGHT)
 
     l0 = L0 / 100.
     l1 = L1 / 100.
@@ -32,36 +34,80 @@ def RunSingleElectronBathtubSim():
     zMax = l0 / np.tan(thetaBot)
     omegaA = V0 * np.sin(thetaBot) / l0
 
+    axFreq = omegaA * ( 1 + l1 * np.tan(thetaBot)/(np.pi*l0) )**-1
+    
     t1 = l1 / (V0 * np.cos(thetaBot))                                                               
     t2 = t1 + np.pi / omegaA
     t3 = t1 + t2
     T  = 2 * t2
     
-    nSteps = 10000
-    maxTime = T*5
+    nSteps = 20000
+    maxTime = 5*T
     timeStepSize = maxTime / nSteps
 
-    print("Cyclotron frequency = ", func.CalcCyclotronFreq(BBKG, EKE) )
+    print('Cyclotron frequency = ', func.CalcCyclotronFreq(BBKG, EKE) )
+    print('Axial frequency = ', axFreq)
     
-    TimeArray    = np.zeros(nSteps)
-    BFieldArray  = np.zeros(nSteps)
-
-    time = 1 * 10**-9
+    TimeArray   = np.zeros(nSteps)
+    BFieldArray = np.zeros(nSteps)
+    AngCyclFreqArray = np.zeros(nSteps)
+    AngCyclFreqArrayDoppler = np.zeros(nSteps)
+    
+    ZVelArray = np.zeros(nSteps)
+    ZPosArray = np.zeros(nSteps)
+    
+    time = 1 * 10**-10
 
     
     for i in range(nSteps):
         TimeArray[i] = time
         BFieldArray[i] = func.BFieldBathtubFromTime(time, V0, L0, L1, B0, BBKG, TRAPDEPTH)
+        AngCyclFreqArray[i] = func.AngCyclFreqBathtubFromTime(time, V0, L0, L1, B0, BBKG, TRAPDEPTH)
 
+        ZPosArray[i], ZVelArray[i] = func.ZPosVelBathtubFromTime(time, V0, L0, L1, B0, BBKG, TRAPDEPTH)
+
+        pos = np.array([0., ZPosArray[i]])
+        vel = np.array([0., ZVelArray[i]])
+        electronDetVec = np.subtract(DETPOS, pos)
+        electronDetVec_hat = electronDetVec / np.linalg.norm(electronDetVec)
+        # Velocity in direction of receiver
+        velReceiver = np.dot(vel, electronDetVec_hat)
+        AngCyclFreqArrayDoppler[i] = func.ReceiverFreqDoppler(ZVelArray[i], AngCyclFreqArray[i])
+        
         time = time + timeStepSize
         
     # Now make the plots
     fig0, ax0 = pyplot.subplots(nrows=2, ncols=1, figsize=[6, 11])
     f0graph0 = ax0[0].plot(TimeArray, BFieldArray)
-
+    f0graph1 = ax0[1].plot(TimeArray, AngCyclFreqArray)
     ax0[0].set_title('Magnetic field experienced')
     ax0[0].set_xlabel('t [s]')
     ax0[0].set_ylabel('B [T]')
+    ax0[1].set_title('Magnetic field experienced')
+    ax0[1].set_xlabel('t [s]')
+    ax0[1].set_ylabel(r'$\Omega_{c}$ [radians $s^{-1}$]')
+
+    fig1, ax1 = pyplot.subplots(nrows=2, ncols=1, figsize=[6, 11])
+    f1graph0 = ax1[0].plot(TimeArray, ZPosArray)
+    f1graph1 = ax1[1].plot(TimeArray, ZVelArray)
+    ax1[0].set_title('Z position')
+    ax1[0].set_xlabel('t [s]')
+    ax1[0].set_ylabel('z [m]')
+    ax1[1].set_title('Z velocity')
+    ax1[1].set_xlabel('t [s]')
+    ax1[1].set_ylabel('$v_{z}$ [m$s^{-1}$]')
+
+    fig2, ax2 = pyplot.subplots(nrows=2, ncols=1, figsize=[6, 11])
+    f2graph0 = ax2[0].hist(AngCyclFreqArray, bins=200)
+    ax2[0].set_title('Angular frequency - no doppler effect')
+    ax2[0].set_xlabel(r'$\Omega_{c}$ [radians $s^{-1}$]')
+    ax2[0].set_ylabel('A. U.')
+ #   ax2[0].set_xlim([1.694e11, 1.696e11])
+    f2graph0 = ax2[1].hist(AngCyclFreqArrayDoppler, bins=200)
+    ax2[1].set_title('Angular frequency - doppler effect')
+    ax2[1].set_xlabel(r'$\Omega_{c}$ [radians $s^{-1}$]') 
+    ax2[1].set_ylabel('A. U.')
+#    ax2[1].set_xlim([1.694e11, 1.696e11])
     
     pyplot.show()
     
--- functions.py
+++ functions.py
@@ -110,3 +110,64 @@ def BFieldBathtubFromTime(time, v0, l0, l1, b0, bBkg, trapDepth):
         field = b0 * ( 1 + zMax*zMax/(2*l0*l0) - zMax*zMax/(2*l0*l0) * np.cos(2*omegaA*(tRem-t3)) )     
 
     return field
+
+def AngCyclFreqBathtubFromTime(time, v0, l0, l1, b0, bBkg, trapDepth):
+    l0 = l0 / 100.
+    l1 = l1 / 100.
+    thetaBot = np.arcsin(np.sqrt(1 - trapDepth/bBkg))
+    zMax = l0 / np.tan(thetaBot)
+    omegaA = v0 * np.sin(thetaBot) / l0
+    gamma = 1 / np.sqrt( 1 - v0*v0/(constant.CLIGHT*constant.CLIGHT) )
+    
+    premult = constant.COULOMBCHARGE * b0 / (gamma * constant.ERESTMASS)
+    
+    t1 = l1 / (v0 * np.cos(thetaBot))
+    t2 = t1 + np.pi / omegaA
+    t3 = t1 + t2
+    T  = 2 * t2
+
+    tRem = time % T
+
+    freq = 0.
+    
+    if tRem > 0. and tRem < t1 :
+        freq = premult
+    elif tRem > t1 and tRem < t2 :
+        freq = premult * ( 1 + zMax*zMax/(2*l0*l0) - zMax*zMax/(2*l0*l0) * np.cos(2*omegaA*(tRem-t1)) )
+    elif tRem > t2 and tRem < t3 :
+        freq = premult 
+    elif tRem > t3 and tRem < T :
+        freq = premult * ( 1 + zMax*zMax/(2*l0*l0) - zMax*zMax/(2*l0*l0) * np.cos(2*omegaA*(tRem-t3)) ) 
+
+    return freq
+
+def ZPosVelBathtubFromTime(time, v0, l0, l1, b0, bBkg, trapDepth):
+    l0 = l0 / 100.
+    l1 = l1 / 100.
+    thetaBot = np.arcsin(np.sqrt(1 - trapDepth/bBkg))
+    vz0 = v0 * np.cos(thetaBot)
+    omegaA = v0 * np.sin(thetaBot) / l0
+    zMax = l0 / np.tan(thetaBot)
+        
+    t1 = l1 / (v0 * np.cos(thetaBot))
+    t2 = t1 + np.pi / omegaA
+    t3 = t1 + t2
+    T  = 2 * t2
+
+    tRem = time % T
+    
+    zPos = 0.
+    zVel = 0.
+    if tRem > 0. and tRem < t1 :
+        zPos = vz0 * tRem - l1/2.
+        zVel = vz0
+    elif tRem > t1 and tRem < t2 :
+        zPos = zMax * np.sin( omegaA*(tRem-t1) ) + l1/2.
+        zVel = zMax * omegaA * np.cos( omegaA*(tRem - t1) )
+    elif tRem > t2 and tRem < t3 :
+        zPos = -vz0 * (tRem - t2) + l1/2.
+        zVel = -vz0
+    elif tRem > t3 and tRem < T :
+        zPos = -zMax * np.sin( omegaA*(tRem-t3) ) - l1/2.
+        zVel = -zMax * omegaA * np.cos( omegaA*(tRem - t3) )
+    return zPos, zVel
